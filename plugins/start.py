5216cdd37fb5827f016bd82784f64ecc2674c25b
KeralaMallu <leooeditzz@gmail.com>
2025-01-13 13:56:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,6 +12,7 @@ from utils import temp, get_shortlink
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
+    await message.react(emoji="❤️‍🔥")
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
