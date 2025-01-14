f627ce7af40e8e50a879ce2595a70d015de54450
KeralaMallu <leooeditzz@gmail.com>
2025-01-14 10:44:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,16 +15,25 @@ PORT = environ.get("PORT", "8080")
 # Image URL for the start command
 START_IMAGE_URL = environ.get("START_IMAGE_URL", "https://envs.sh/vp4.jpg")
 
+# Online Stream and Download
+MULTI_CLIENT = False
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+ON_HEROKU = 'DYNO' in environ
+
+# URL for the bot (should be set in environment variables)
+URL = environ.get("URL", "")
+
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002441553603'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7577977996').split()]
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002393199149 -1002298889599').split()] 
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002393199149 -1002298889599').split()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cristi7jjr:tRjSVaoSNQfeZ0Ik@cluster0.kowid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "FileToLink")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', False)) 
+SHORTLINK = bool(environ.get('SHORTLINK', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
