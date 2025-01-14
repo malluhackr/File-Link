6daf0e771a886f311268dea65b2d10badfe30b8f
KeralaMallu <leooeditzz@gmail.com>
2025-01-14 11:06:25 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,4 +1,3 @@
-import re
 import motor.motor_asyncio
 from info import DATABASE_NAME, DATABASE_URI
 
@@ -11,8 +10,9 @@ class Database:
 
     def new_user(self, id, name):
         return dict(
-            id = id,
-            name = name,
+            id=id,
+            name=name,
+            banned=False  # Default to not banned
         )
     
     async def add_user(self, id, name):
@@ -20,7 +20,7 @@ class Database:
         await self.col.insert_one(user)
     
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
+        user = await self.col.find_one({'id': int(id)})
         return bool(user)
     
     async def total_users_count(self):
@@ -32,5 +32,26 @@ class Database:
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
+
+    async def ban_user(self, user_id):
+        """Ban a user by updating their banned status in the database."""
+        result = await self.col.update_one(
+            {'id': int(user_id)}, 
+            {'$set': {'banned': True}}
+        )
+        return result.modified_count > 0
+
+    async def unban_user(self, user_id):
+        """Unban a user by updating their banned status in the database."""
+        result = await self.col.update_one(
+            {'id': int(user_id)}, 
+            {'$set': {'banned': False}}
+        )
+        return result.modified_count > 0
+
+    async def is_user_banned(self, user_id):
+        """Check if a user is banned."""
+        user = await self.col.find_one({'id': int(user_id)})
+        return user['banned'] if user else False
 
 db = Database(DATABASE_URI, DATABASE_NAME)
