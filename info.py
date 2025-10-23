615ab988c8740547ae14d9503a31205a532d2ddd
malluhackr <cristi7jjr@gmail.com>
2025-10-23 21:52:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,6 +32,6 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cristi7jjr:tRjSVaoSNQf
 DATABASE_NAME = environ.get('DATABASE_NAME', "FileToLink")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
+SHORTLINK = bool(environ.get('SHORTLINK', true)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
