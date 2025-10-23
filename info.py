39e1f4ef917fc95fffd8dfa325de3b8d1537b152
malluhackr <cristi7jjr@gmail.com>
2025-10-23 22:40:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,5 +28,10 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002393199149').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cristi7jjr:tRjSVaoSNQfeZ0Ik@cluster0.kowid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "FileToLink")
+
+# Shortlink Info
+SHORTLINK = bool(environ.get('SHORTLINK', 'false'))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
