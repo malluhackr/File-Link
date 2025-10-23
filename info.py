8f462c5b3058d1e3e185748c05bb40c79e2dbff0
malluhackr <cristi7jjr@gmail.com>
2025-10-23 22:13:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,8 +30,3 @@ AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cristi7jjr:tRjSVaoSNQfeZ0Ik@cluster0.kowid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "FileToLink")
-
-# Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', 'false'))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
